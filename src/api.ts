49230ed24f2b33721cb033c0cad1366cd794b7e7
conjuncts <67614673+conjuncts@users.noreply.github.com>
2025-02-24 09:50:19 -0600

    use proxy

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import { ComicData } from "./types";
 
 export async function fetchComic(id?: number): Promise<ComicData> {
     // do not use proxy
-    const proxyUrl = ''; // https://corsproxy.io/?url='; // Example proxy
+    const proxyUrl = 'https://corsproxy.io/?url='; // Example proxy
 
     const url = id 
         ? `https://xkcd.com/${id}/info.0.json`
